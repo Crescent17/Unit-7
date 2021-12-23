32254ba998483ae9b2cd9901cc4125fa00537a45
Marat <94042173+Crescent17@users.noreply.github.com>
2021-12-23 16:48:48 +0300

    Add files via upload

---
+++ Encapsulation/src/Printer.java
@@ -0,0 +1,40 @@
+public class Printer {
+    private int tonerLevel;
+    private int pagesPrinted;
+    private boolean duplex;
+
+    public Printer(int tonerLevel, boolean duplex) {
+        if (tonerLevel > -1 && tonerLevel <= 100) {
+            this.tonerLevel = tonerLevel;
+        } else {
+            this.tonerLevel = -1;
+        }
+        this.duplex = duplex;
+        this.pagesPrinted = 0;
+    }
+
+    public int getPagesPrinted() {
+        return pagesPrinted;
+    }
+
+    public int addToner(int tonerAmount) {
+        if (tonerAmount > 0 && tonerAmount <= 100) {
+            if (tonerLevel + tonerAmount <= 100) {
+                tonerLevel += tonerAmount;
+                return tonerLevel;
+            }
+            return -1;
+        }
+        return -1;
+    }
+
+    public int printPages(int pages) {
+        int pagesToPrint = pages;
+        if (duplex) {
+            System.out.println("Duplex mode");
+            pagesToPrint = pages / 2 + pages % 2;
+        }
+        this.pagesPrinted += pagesToPrint;
+        return pagesToPrint;
+    }
+}
